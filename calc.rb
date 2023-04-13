ce1343728551ef2995b6729e705c28a1b4cefe47
Hemili Constantino <82230000+hemilir@users.noreply.github.com>
2023-04-13 04:16:16 -0300

    Aula 1, 2 e 3

---
+++ calc.rb
@@ -0,0 +1,17 @@
+print "Digite o primeiro número inteiro: "
+
+number1 = gets.chomp.to_i
+
+print "Digite o segundo número inteiro: "
+
+number2 = gets.chomp.to_i
+
+addition = number1 + number2
+
+subtraction = number1 - number2
+
+division = number1 / number2
+
+multiplication = number1 * number2
+
+puts "O resultado da adição, subtração, divisão e multiplicação entre os dois números é: #{addition}, #{subtraction}, #{division} e #{multiplication}"
